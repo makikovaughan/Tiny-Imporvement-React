16b56883e7cffaa8954724818e6f310a8143260c
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-12-02 14:13:19 -0500

    Modal changes

--- client/src/components/KudoModal.js
+++ client/src/components/KudoModal.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import * as $ from 'axios';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from 'reactstrap';
 
 class KudoModal extends React.Component {
-    state = {
-      modal: false
-    }
+  state = {
+    modal: false,
+    userList: []
+  }
 
   toggle = () => {
     this.setState({
@@ -12,14 +28,59 @@ class KudoModal extends React.Component {
     });
   }
 
+  getUserList = () => {
+    $.get(`/api/users`)
+      .then((result) => {
+        console.log(result);
+        this.setState({ userList: result.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  //Execute this when the mount is done.
+  componentDidMount() {
+    this.getUserList();
+  }
+
   render() {
     return (
       <div>
         <Button color="info" onClick={this.toggle}>Give Kudos</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} size="lg">
           <ModalHeader className="text-info" toggle={this.toggle}>Give a Kudos</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Container>
+              <Row>
+                <Col xs="12" md="3">
+                  <Form>
+                    <Input type="select" name="fromSelect" className="custom-select btn btn-info" id="fromSelect">
+                      <option selected>From</option>
+                      {
+                        this.state.userList.map((user) => (
+                          <option id={user._id}>{user.username}</option>
+                        ))
+                      }
+                    </Input>
+                  </Form>
+                </Col>
+                <Col xs="12" md="5">
+                </Col>
+                <Col xs="12" md="3">
+                  <Form>
+                    <Input type="select" name="toSelect" className="custom-select btn btn-info" id="toSelect">
+                      <option selected>To</option>
+                      {
+                        this.state.userList.map((user) => (
+                          <option id={user._id}>{user.username}</option>
+                        ))
+                      }
+                    </Input>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
           </ModalBody>
           <ModalFooter>
             <Button color="info" onClick={this.toggle}>Send Kudos</Button>
