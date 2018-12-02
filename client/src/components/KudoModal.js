4a7adc064ba7e542efc8fd2fd0f8e9b6af4105e5
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-12-02 17:31:20 -0500

    Added post

--- client/src/components/KudoModal.js
+++ client/src/components/KudoModal.js
@@ -10,22 +10,37 @@ import {
   Row,
   Col,
   Form,
-  FormGroup,
   Label,
   Input,
-  FormText
 } from 'reactstrap';
+
+const Users = (props) => (
+
+  props.userList.map((user) => (
+    <option key={user._id} id={user._id} value={user._id}>{user.username}</option>
+  ))
+
+);
 
 class KudoModal extends React.Component {
   state = {
     modal: false,
-    userList: []
+    userList: [],
+    isError: false,
+    title: "",
+    body: "",
+    from_user: "",
+    to_user: ""
   }
 
+  //Close&Open Modal
   toggle = () => {
     this.setState({
       modal: !this.state.modal
     });
+    if (this.state.modal) {
+      window.location.reload();
+    }
   }
 
   getUserList = () => {
@@ -39,6 +54,56 @@ class KudoModal extends React.Component {
       });
   }
 
+  postKudos = (newKudo) => {
+    console.log(newKudo);
+    $.post(`/api/kudos`, newKudo)
+    .then((result)=>{
+      console.log(result);
+      this.toggle();
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+    window.location.reload();
+  }
+
+  changeHandler = (event) => {
+
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+
+  }
+
+  clickHandler = (event) => {
+    event.preventDefault();
+    const newKudo = {
+      title: this.state.title,
+      body: this.state.body,
+      from_user: this.state.from_user,
+      to_user: this.state.to_user
+    }
+
+    console.log(newKudo);
+    if (newKudo.title === "" || newKudo.body === "" || newKudo.from_user === "" || newKudo.to_user === "") {
+      this.setState({
+        isError: true
+      });
+    }
+    else if (newKudo.from_user === newKudo.to_user) {
+      this.setState({
+        isError: true
+      });
+    }
+    else {
+      console.log("Here");
+      this.postKudos(newKudo);
+      this.setState({
+        isError: false
+      });
+    }
+  }
+
   //Execute this when the mount is done.
   componentDidMount() {
     this.getUserList();
@@ -50,32 +115,41 @@ class KudoModal extends React.Component {
         <Button color="info" onClick={this.toggle}>Give Kudos</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} size="lg">
           <ModalHeader className="text-info" toggle={this.toggle}>Give a Kudos</ModalHeader>
-          <ModalBody>
+          <ModalBody className="text-info">
             <Container>
+              {
+                this.state.isError ?
+                  (<Row>
+                    <Col xs="12">
+                      <div className="alert alert-danger">
+                        Please check the input.
+                  </div>
+                    </Col>
+                  </Row>) : <span />
+              }
               <Row>
                 <Col xs="12" md="3">
                   <Form>
-                    <Input type="select" name="fromSelect" className="custom-select btn btn-info" id="fromSelect">
-                      <option selected>From</option>
-                      {
-                        this.state.userList.map((user) => (
-                          <option id={user._id}>{user.username}</option>
-                        ))
-                      }
+                    <Input type="select" onChange={this.changeHandler} name="from_user" className="custom-select btn btn-info" id="fromSelect" value={this.state.from_user}>
+                      <option id="from">From</option>
+                      <Users userList={this.state.userList} />
                     </Input>
                   </Form>
                 </Col>
                 <Col xs="12" md="5">
+                  <Form>
+                    <Label for="kudoTitle">Kudo Title</Label><br />
+                    <Input type="text" onChange={this.changeHandler} name="title" id="kudoTitle" value={this.state.title} placeholder="Please type the title" />
+                    <br /><br />
+                    <Label for="kudoMessage">Kudo Message</Label><br />
+                    <Input type="textarea" onChange={this.changeHandler} name="body" id="kudoMessage" rows="4" cols="30" value={this.state.body} placeholder="Please type the Kudo message." />
+                  </Form>
                 </Col>
                 <Col xs="12" md="3">
                   <Form>
-                    <Input type="select" name="toSelect" className="custom-select btn btn-info" id="toSelect">
-                      <option selected>To</option>
-                      {
-                        this.state.userList.map((user) => (
-                          <option id={user._id}>{user.username}</option>
-                        ))
-                      }
+                    <Input type="select" onChange={this.changeHandler} name="to_user" value={this.state.to_user} className="custom-select btn btn-info" id="toSelect">
+                      <option id="to">To</option>
+                      <Users userList={this.state.userList} />
                     </Input>
                   </Form>
                 </Col>
@@ -83,7 +157,7 @@ class KudoModal extends React.Component {
             </Container>
           </ModalBody>
           <ModalFooter>
-            <Button color="info" onClick={this.toggle}>Send Kudos</Button>
+            <Button color="info" onClick={this.clickHandler}>Send Kudos</Button>
             <Button color="secondary" onClick={this.toggle}>Close</Button>
           </ModalFooter>
         </Modal>
